08fa7efc5a451f9f578db514f04c6a2963f81e62
Shunmugapragasam S <95437100+disys-shunmuga@users.noreply.github.com>
2021-12-13 11:14:29 +0530

    Add files via upload

---
+++ phone_tracking.py
@@ -0,0 +1,179 @@
+class phonetracking:
+    def __init__(self):
+        self.Watsapp   = {"screen_on_time":"27mins", "notification_recived":"135 notifications","times_opened":"50times"}
+        self.Instagram ={"screen_on_time":"2hr_27mins","notification_recived":"24 notifications","times_opened":"60times"}
+        self.Phone     ={"screen_on_time":"1hr_14mins","notification_recived":"60 notifications","times_opened":"59times"}
+        self.Youtube   ={"screen_on_time":"1hr_7mins","notification_recived":"5 notifications","times_opened":"9times"}
+        print("Time used  in a day \n")
+        
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Instagram --",self.Instagram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Instagram --",self.Instagram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Instagram --",self.Instagram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+        
+    def monday(self):
+        self.Watsapp   = {"screen_on_time":"32mins", "notification_recived":"214 notifications","times_opened":"48times"}
+        self.Instagram ={"screen_on_time":"58mins","notification_recived":"14 notifications","times_opened":"22times"}
+        self.Phone     ={"screen_on_time":"14mins","notification_recived":"30 notifications","times_opened":"32times"}
+        self.Youtube   ={"screen_on_time":"1hr_3mins","notification_recived":"5 notifications","times_opened":"7times"}
+        print("Time used  in a day \n")
+        
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Instagram --",self.Instagram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Instagram --",self.Instagram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Instagram --",self.Instagram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+        
+    def tuesday(self):
+        self.Watsapp   = {"screen_on_time":"27mins", "notification_recived":"169 notifications","times_opened":"57times"}
+        self.Instagram ={"screen_on_time":"58mins","notification_recived":"18 notifications","times_opened":"25times"}
+        self.Phone     ={"screen_on_time":"10mins","notification_recived":"27 notifications","times_opened":"44times"}
+        self.Youtube   ={"screen_on_time":"16mins","notification_recived":"5 notifications","times_opened":"6times"}
+        print("Time used  in a day \n")
+        
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Instagram --",self.Instagram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Instagram --",self.Instagram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Instagram --",self.Instagram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+        
+    def wednesday(self):
+        self.Watsapp   = {"screen_on_time":"27mins", "notification_recived":"155 notifications","times_opened":"80times"}
+        self.Instagram ={"screen_on_time":"55mins","notification_recived":"16 notifications","times_opened":"55times"}
+        self.Phone     ={"screen_on_time":"1hr_30mins","notification_recived":"74 notifications","times_opened":"22times"}
+        self.Youtube   ={"screen_on_time":"55mins","notification_recived":"5 notifications","times_opened":"3times"}
+        print("Time used  in a day \n")
+        
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Instagram --",self.Instagram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Instagram --",self.Instagram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Instagram --",self.Instagram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+        
+    def thursday(self):
+        self.Watsapp   = {"screen_on_time":"60mins", "notification_recived":"122 notifications","times_opened":"48times"}
+        self.Instagram ={"screen_on_time":"1hr_17mins","notification_recived":"37 notifications","times_opened":"61times"}
+        self.Phone     ={"screen_on_time":"1hr_34mins","notification_recived":"66 notifications","times_opened":"74times"}
+        self.Youtube   ={"screen_on_time":"57mins","notification_recived":"5 notifications","times_opened":"22times"}
+        print("Time used  in a day \n")
+        
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Instagram --",self.Instagram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Instagram --",self.Instagram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Instagram --",self.Instagram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+        
+    def friday(self):
+        self.Watsapp   = {"screen_on_time":"45mins", "notification_recived":"177 notifications","times_opened":"85times"}
+        self.Instagram ={"screen_on_time":"1hr_18mins","notification_recived":"65 notifications","times_opened":"44times"}
+        self.Phone     ={"screen_on_time":"1hr_18mins","notification_recived":"69 notifications","times_opened":"89times"}
+        self.Youtube   ={"screen_on_time":"1hr_57mins","notification_recived":"5 notifications","times_opened":"35times"}
+        print("Time used  in a day \n")
+        
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Instagram --",self.Instagram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Instagram --",self.Instagram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Instagram --",self.Instagram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+        
+    def saturday(self):
+        self.Watsapp   = {"screen_on_time":"27mins", "notification_recived":"135 ","times_opened":"50times"}
+        self.Instagram ={"screen_on_time":"2hr_27mins","notification_recived":"24 ","times_opened":"60times"}
+        self.Phone     ={"screen_on_time":"1hr_14mins","notification_recived":"60 ","times_opened":"59times"}
+        self.Youtube   ={"screen_on_time":"1hr_7mins","notification_recived":"5 ","times_opened":"9times"}
+        print("Time used  in a day \n")
+        
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Instagram --",self.Instagram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Instagram --",self.Instagram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Instagram --",self.Instagram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+
+    
+ph = phonetracking()
+ph.monday()
+ph.tuesday()
+ph.wednesday()
+ph.thursday()
+ph.friday()
+ph.saturday()
+
+                      
